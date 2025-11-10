3d5e9a012c8df9049112d97c03b9ac7917dc78c0
Igor Bolduev <101598982+igorbolduev@users.noreply.github.com>
2025-11-10 19:43:30 +0200

    добавляю кнопку закрытия модалки

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 const modalButton = document.querySelector(".modal__button");
 const modal = document.querySelector(".modal");
+const modalClose = document.createElement("button");
 
 modalButton.addEventListener("click", () => {
   modal.style.display = "flex";
@@ -12,3 +13,12 @@ modal.addEventListener("click", (event) => {
     modal.style.display = "none";
   }
 });
+
+modalClose.textContent = "X";
+modalClose.classList.add("modalClose");
+
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.appendChild(modalClose);
